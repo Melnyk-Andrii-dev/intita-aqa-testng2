2bcdebbb9fbc7d6189dfd3fc5992513986fbf050
Администратор <sao2099@gmail.com>
2022-05-03 21:19:07 +0300

    Initial commit

---
+++ src/test/java/org/intita/AppTest.java
@@ -0,0 +1,18 @@
+package org.intita;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+
+    @Test
+    public void test(){
+        Assert.assertTrue(true);
+    }
+
+}
